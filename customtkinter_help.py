2e32a1aac1c4929bcb87f0fcb9df01a7c57703d5
Aleksutka <alex.geiz@outlook.com>
2026-05-05 00:12:13 +0300

    Add files via upload
    updated file which can do any expressions

--- customtkinter_help.py
+++ customtkinter_help.py
@@ -1,24 +1,29 @@
 import customtkinter as ctk
 
-def terminal_output():
-    written = inp.get()
-    textInWindow.configure(text=f"You wrote: {written}")
-
 app = ctk.CTk()
 app.title("cudtomTkinter App")
 app.geometry("320x520")
 
+def terminal_output():
+    written = inp.get()
+    try:
+        result = eval(written)
+        textInWindow.configure(text=f"{result}")
+    except ZeroDivisionError:
+        textInWindow.configure(text="Yue can't devide by 0!")
+    except:
+        textInWindow.configure(text="Ivalid expression")
 
 textInWindow = ctk.CTkLabel(app, text="Result will be here")
-textInWindow.grid(row=0, pady=(10, 20), padx=20)
+textInWindow.grid(row=0, pady=(10, 10), padx=20)
 
-title = ctk.CTkLabel(app, text="Write something down:")
+title = ctk.CTkLabel(app, text="Write an expression:")
 title.grid(row=1, pady=(10, 10), padx=20)
 
 inp = ctk.CTkEntry(app, placeholder_text="Write here", width=200)
 inp.grid(row=2, pady=(10, 10), padx=20)
 
-btn = ctk.CTkButton(app, text="Click here to appear your writtentext", command=terminal_output)
+btn = ctk.CTkButton(app, text="Click here to appear your result", command=terminal_output)
 btn.grid(row=3, padx=20)
 
 app.mainloop()
