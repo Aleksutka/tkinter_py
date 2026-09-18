a8e4ee93333e7cf5766fa1ddd4180cef622e2ba3
Aleksutka <alex.geiz@outlook.com>
2026-09-18 14:09:48 +0300

    Add files via upload

--- tel_Numbers_window.py
+++ tel_Numbers_window.py
@@ -5,31 +5,49 @@ app = ctk.CTk()
 app.title("Telefona nummuru grāmatiņa")
 app.geometry("420x520")
 app.grid_columnconfigure(0, weight=1)
-app.grid_columnconfigure(1, weight=1)
 app.resizable(width=False, height=False)
 
+tagad = "name"
+
 def pievienot():
+    global tagad
+    tagad = "name"
     txt.configure(text="Ievadiet lototāju!")
-    inp.bind("<Return>", ievadiet_vardu)
+    inp.bind("<Return>", queue)
 
-def ievadiet_vardu(event):
-    global name
-    name = inp.get().strip()
-    print(name)
-    inp.delete(0, "end")
-    txt.configure(text="Ievadiet telefona numuru!")
-    inp.bind("<Return>", ievadiet_numuru)
+def queue(event):
+    global tagad, name
+    if tagad == "name":
+        name = inp.get().strip()
+        print(name)
+        inp.delete(0, "end")
+        txt.configure(text="Ievadiet telefona numuru!")
+        tagad = "number"
+    elif tagad == "number":
+        number = inp.get().strip()
+        inp.delete(0, "end")
+        try:
+            with open(f"{name}.txt", "x") as f:
+                f.write(number)
+            txt.configure(text="Jūs pievienojāt jaunu litotāja nummuru")
+        except FileExistsError:
+            txt.configure(text="Tāds vārds jau eksistē!")
+        tagad = "name"
 
-def ievadiet_numuru(event):
-    global number
-    number = inp.get().strip()
-    inp.delete(0, "end")
+def atrast():
+    txt.configure(text="Ievadiet vārdu!")
+    inp.bind("<Return>", mekle)
+
+def mekle(event):
     try:
-        with open(f"{name}.txt", "x") as f:
-            f.write(number)
-        txt.configure(text="Jūs pievienojāt jaunu litotāja nummuru")
-    except FileExistsError:
-        txt.configure(text="Tāds vārds jau eksistē!")
+        nameA = inp.get().strip()
+        print(nameA)
+        inp.delete(0, "end")
+        with open(f"{nameA}.txt") as f:
+            txt.configure(text=f.read())
+    except FileNotFoundError:
+        txt.configure(text="Tāda nav vēl kontakta!")
+        
 
 
 txt = ctk.CTkLabel(app, text="", font=("Arial", 20))
@@ -39,9 +57,9 @@ inp = ctk.CTkEntry(app, justify="center", placeholder_text="", width=200, height
 inp.grid(row=2, pady=20)
 
 btnPie = ctk.CTkButton(app, text="Pievienot nummuru", command=pievienot, height=35, width=200)
-btnPie.grid(row=3, column=0, padx=5, pady=5)
+btnPie.grid(row=3, padx=5, pady=5)
 
-btnAtr = ctk.CTkButton(app, text="Atrast nummuru pēc vārda", height=35, width=200)
-btnAtr.grid(row=3, column=1, padx=5, pady=5)
+btnAtr = ctk.CTkButton(app, text="Atrast nummuru pēc vārda", command=atrast, height=35, width=200)
+btnAtr.grid(row=4, padx=5, pady=5)
 
 app.mainloop()
